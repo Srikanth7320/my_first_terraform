bdc1e98c9d414c0339719d1a3481b6e4e76c0220
srikanth <venkatasrikanthkv@gmail.com>
2026-03-04 20:43:13 +0530

    My first Terraform file is writted in aws/local_state

---
+++ aws/local_state/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-2"
+}
+
+
+resource "aws_instance" "Terraform-Demo" {
+  # This is the specific ID for Ubuntu 24.04 LTS in us-east-1
+  # Note: This ID changes every time Canonical releases a security patch!
+  ami           = "ami-04a81a99f5ec58529" 
+  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Ubuntu24-Manual"
+  }
+}
